1b91c288359d54d5c83c4875c954ffb66739442b
Yoon <ysj0509@sookmyung.ac.kr>
2024-11-24 01:30:17 +0900

    Style: 수업 어플리케이션명을 'course'로 수정
    - 'class'는 키워드와 중복되므로 'course'로 변경함

--- BE/settings.py
+++ BE/settings.py
@@ -36,9 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'class.apps.KubelabxConfig',
+    'course.apps.KubelabxConfig',
 
     'user',
+    'course',
 ]
 
 MIDDLEWARE = [
--- course/apps.py
+++ course/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class KubelabxConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'class'
+    name = 'course'
