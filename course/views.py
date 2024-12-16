053209cab2ac64b5a3d359f79dc77873ed15729d
Yoon <ysj0509@sookmyung.ac.kr>
2024-12-16 12:29:16 +0900

    Format: 코드 포맷팅
    - 불필요 주석 삭제
    - 줄간격 통일

--- course/views.py
+++ course/views.py
@@ -1,8 +1,6 @@
 import json
-from venv import logger
 from django.views.decorators.csrf import csrf_exempt
 from course.models import Course
-from user.models import User
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
@@ -12,23 +10,17 @@ from practice.models import StudentPod
 
 
 @csrf_exempt
-# JWT 토큰 인증 요구
 @authentication_classes([JWTAuthentication])
-#인증된 사용자만 -> 추후 인증 불필요
 @permission_classes([IsAuthenticated])
-#POST만 허용(REST API 뷰용)
 @api_view(['POST'])
 def create_course(request):
     try:
-        #교수님인 경우만
         if request.user.user_type != 't':
             return JsonResponse({"error": "Only teachers can create courses"}, status=403)
         
         data = json.loads(request.body)
-        #data의 이름 추출
         name = data.get('name')
         
-        #이름이 공백인 경우
         if not name or not name.strip():
             return JsonResponse({"error": "Course name is required"}, status=400)
         
@@ -37,7 +29,7 @@ def create_course(request):
             name=name.strip(),
             teacher=request.user
         )
-        config.load_incluster_config()  # Kubernetes 클러스터 설정 로드
+        config.load_incluster_config()
         v1 = client.CoreV1Api()
         namespace_manifest = {
             "apiVersion": "v1",
@@ -46,6 +38,9 @@ def create_course(request):
                 "name": f"course-{course.id}"
             }
         }
+        if any(ns.metadata.name == namespace_manifest['metadata']['name'] for ns in v1.list_namespace().items):
+            return JsonResponse({"error": "Namespace already exists"}, status=400)
+
         v1.create_namespace(body=namespace_manifest)
 
         return JsonResponse({
@@ -60,12 +55,11 @@ def create_course(request):
         course.delete()
         return JsonResponse({"error": f"Failed to create k8s namespace: {e}"}, status=500)
 
-    # 잘못된 형식
     except json.JSONDecodeError:
         return JsonResponse({"error": "Invalid JSON format"}, status=400)
-    # 서버 오류
-    except Exception:
-        return JsonResponse({"error": "Internal server error"}, status=500)
+
+    except Exception as e:
+        return JsonResponse({"error": f"Internal server error: {e}"}, status=500)
 
 
 @csrf_exempt
@@ -74,12 +68,10 @@ def create_course(request):
 @permission_classes([IsAuthenticated])
 def list_up_courses(request):
     try:
-        #반환을 위한 request 사용자명, 사용자 타입 추출
         user_name = request.user.first_name
         user_role = request.user.user_type
 
-        # 역할에 따라서
-        if user_role == 't': #교수인 경우
+        if user_role == 't':
             courses = Course.objects.filter(teacher=request.user)
             course_data = [{
                 'id': c.id,
@@ -88,7 +80,7 @@ def list_up_courses(request):
                 'created_at': c.created_at,
             } for c in courses]
 
-        elif user_role == 's': #학생인 경우
+        elif user_role == 's':
             courses = Course.objects.filter(participants=request.user)
             course_data = [{
                 'id': c.id,
@@ -97,11 +89,8 @@ def list_up_courses(request):
                 'created_at': c.created_at,
             } for c in courses]
 
-        else: #그외 처리
-            return JsonResponse(
-                {"error": "Invalid user role"}, 
-                status=400
-            )
+        else:
+            return JsonResponse({"error": "Invalid user role"}, status=400)
         
         return JsonResponse({
             "courses": course_data,
@@ -110,11 +99,7 @@ def list_up_courses(request):
         }, status=200)
 
     except Exception as e:
-        logger.error(f"Error in list_up_courses: {str(e)}")
-        return JsonResponse(
-            {"error": str(e)}, 
-            status=500
-        )
+        return JsonResponse({"error": f"Failed to create Pod: {e}"}, status=500)
 
 
 @csrf_exempt
@@ -140,35 +125,21 @@ def end_course(request, course_id):
 @permission_classes([IsAuthenticated])
 def register_course(request):
     try:
-        # 교수님은 참여가 불가
         if request.user.user_type == 't':
-            return JsonResponse({
-                "error": "Teachers cannot register for courses"
-            }, status=403)
+            return JsonResponse({"error": "Teachers cannot register for courses"}, status=403)
         
         data = json.loads(request.body)
-        #data의 code 데이터 get
         code = data.get('code')
-
-        #코드형태 점검
         if not code:
-            return JsonResponse({
-                "error": "Course code is required"
-            }, status=400)
-        
-        # 수업 조회
+            return JsonResponse({"error": "Course code is required"}, status=400)
+
         try:
             course = Course.objects.get(code=code)
         except Course.DoesNotExist:
-            return JsonResponse({
-                "error": "Invalid course code"
-            }, status=404)
+            return JsonResponse({"error": "Invalid course code"}, status=404)
 
-        # 중복 참여 확인
         if course.participants.filter(id=request.user.id).exists():
-            return JsonResponse({
-                "error": "You are already registered for this course"
-            }, status=400)
+            return JsonResponse({"error": "You are already registered for this course"}, status=400)
         
         course.participants.add(request.user)
 
@@ -176,11 +147,10 @@ def register_course(request):
         try:
             pod_name = f"{course.name.lower()}-{request.user.id}"
             namespace = f"course-{course.id}"
-            # 이미 Pod 정보가 존재하면 그대로 사용
+
             if StudentPod.objects.filter(student=request.user, course=course).exists():
-                return JsonResponse({"message": "You have already been assigned a Pod.", "pod_name": pod_name},
-                                    status=200)
-            # Kubernetes Pod 생성
+                return JsonResponse({"message": "You have already been assigned a Pod.", "pod_name": pod_name}, status=200)
+
             config.load_incluster_config()
             v1 = client.CoreV1Api()
             pod_manifest = {
@@ -206,7 +176,6 @@ def register_course(request):
                 }
             }
             v1.create_namespaced_pod(namespace=namespace, body=pod_manifest)
-            # DB에 Pod 정보 저장
             StudentPod.objects.create(student=request.user, course=course, pod_name=pod_name)
 
         except client.exceptions.ApiException as e:
@@ -220,15 +189,10 @@ def register_course(request):
         }, status=200)
     
     except json.JSONDecodeError:
-        return JsonResponse({
-            "error": "Invalid request format"
-        }, status=400)
+        return JsonResponse({"error": "Invalid request format"}, status=400)
     
     except Exception as e:
-        logger.error(f"Error in register_course: {str(e)}")
-        return JsonResponse({
-            "error": "Failed to register for course"
-        }, status=500)
+        return JsonResponse({"error": f"Failed to register for course: {e}"}, status=500)
 
 
 @api_view(['GET'])
@@ -238,15 +202,15 @@ def enter_course(request, course_id):
     try:
         user = request.user
         course = Course.objects.get(id=course_id)
-
         if user.user_type == 't' and course.teacher != user:
             return JsonResponse({"error": "Unauthorized access"}, status=403)
         if user.user_type == 's' and user not in course.participants.all():
             return JsonResponse({"error": "You are not registered for this course"}, status=403)
+
         return JsonResponse({
             "course_name": course.name,
             "user_name": user.first_name,
-            "course_code" : course.code,  # 현재 로그인한 사용자 이름 추가
+            "course_code" : course.code,
         }, status=200)
 
     except Course.DoesNotExist:
@@ -261,11 +225,9 @@ def get_course_progress(request, course_id):
     try:
         course = Course.objects.get(id=course_id)
         
-        # 교수자 권한 확인
         if request.user != course.teacher:
             return JsonResponse({"error": "Unauthorized access"}, status=403)
             
-        # 학생 목록과 진행률 데이터
         participants_data = [{
             'name': student.first_name,
             'id': student.user_id,
